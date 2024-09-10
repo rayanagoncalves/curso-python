e73e3e91d8f9a9debb45b8f4c3977905dfbce2c6
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-09-10 16:11:14 -0300

    feat: solve exercices

---
+++ exercices.py
@@ -0,0 +1,14 @@
+# Exercicio 1
+print('Python na Escola de Programação da Alura')
+
+# Exercicio 2
+name = 'Rayana'
+age = 25
+print(f'Meu nome é {name} e tenho {age} anos')
+
+# Exercicio 3
+print('A','L','U','R','A',sep='\n')
+
+# Exercicio 4
+pi = 3.14159
+print(f'O valor arredondado de pi é:', round(pi, 2))
