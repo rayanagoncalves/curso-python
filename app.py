7806f9e05abcaab7e1f497475f4e5e66b7653a34
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-09-10 16:47:34 -0300

    feat: organizes the code into functions

--- app.py
+++ app.py
@@ -1,31 +1,41 @@
 import os
 
-print("""
+def show_program_name():
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
 ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
-""")
-
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-option = int(input('Escolha uma opção: '))
-# chosen_option = int(chosen_option)
+    """)
+def show_options(): 
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
 def finish_app():
     os.system('clear')
     print('Encerrando o programa\n')
 
-if option == 1:
-    print('Cadastrar restaurante')
-elif option == 2:
-    print('Listar restaurantes')
-elif option == 3:
-    print('Ativar restaurante')
-else:
-    finish_app()
+def choose_option():
+    option = int(input('Escolha uma opção: '))
+    # chosen_option = int(chosen_option)
+    
+    if option == 1:
+        print('Cadastrar restaurante')
+    elif option == 2:
+        print('Listar restaurantes')
+    elif option == 3:
+        print('Ativar restaurante')
+    else:
+        finish_app()
+
+def main():
+    show_program_name()
+    show_options()
+    choose_option()
+
+if __name__ == '__main__':
+    main()
