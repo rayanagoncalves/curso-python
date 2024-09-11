ac533315a3e5402a4f7d018fdab901102d8ef95c
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-09-11 20:29:36 -0300

    feat: implements try-except

--- app.py
+++ app.py
@@ -19,20 +19,31 @@ def finish_app():
     os.system('clear')
     print('Encerrando o programa\n')
 
+def invalid_option():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal: ')
+    main()
+
 def choose_option():
-    option = int(input('Escolha uma opção: '))
-    # chosen_option = int(chosen_option)
-    
-    if option == 1:
-        print('Cadastrar restaurante')
-    elif option == 2:
-        print('Listar restaurantes')
-    elif option == 3:
-        print('Ativar restaurante')
-    else:
-        finish_app()
+    try:
+        option = int(input('Escolha uma opção: '))
+        # chosen_option = int(chosen_option)
+
+        if option == 1:
+            print('Cadastrar restaurante')
+        elif option == 2:
+            print('Listar restaurantes')
+        elif option == 3:
+            print('Ativar restaurante')
+        elif option == 4:
+            finish_app()
+        else:
+            invalid_option()
+    except:
+        invalid_option()
 
 def main():
+    os.system('cls')
     show_program_name()
     show_options()
     choose_option()
