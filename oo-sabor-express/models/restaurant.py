125b7715f68e8571feff166b20210dfeaeb3fab9
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-09-13 15:50:53 -0300

    chore: solve exercises

--- oo-sabor-express/models/restaurant.py
+++ oo-sabor-express/models/restaurant.py
@@ -5,8 +5,26 @@ class Restaurant:
 
 restaurant_praca = Restaurant()
 restaurant_praca.name = 'Praça'
-restaurant_praca.category = 'Gourmet'
-restaurant_pizza = Restaurant()
+restaurant_praca.category = 'Italiana'
 
-restaurants = [restaurant_praca, restaurant_pizza]
+restaurants = [restaurant_praca]
+
 print(vars(restaurant_praca))
+
+print(f'Nome: {restaurant_praca.name}')
+
+print('Ativo') if restaurant_praca.active else print('Desativado')
+
+category = Restaurant.category
+Restaurant.name = 'Bistrô'
+
+restaurant_pizza = Restaurant()
+restaurant_pizza.name = 'Pizza Place'
+restaurant_pizza.category = 'Fast Food'
+
+if restaurant_pizza.category == 'Fast Food':
+    print(f'Categoria do restaurante {restaurant_pizza.name} é Fast Food!')
+
+restaurant_pizza.active = True
+
+print(f'Nome: {restaurant_praca.name} - Categoria: {restaurant_praca.category}')
