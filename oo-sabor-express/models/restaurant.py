2d7461de8b087b1ac6ac4f4b91b4094b1306da0f
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-09-16 18:07:49 -0300

    feat: create method to list restaurants

--- oo-sabor-express/models/restaurant.py
+++ oo-sabor-express/models/restaurant.py
@@ -1,31 +1,35 @@
 class Restaurant:
+    restaurants = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
         self.active = False
+        Restaurant.restaurants.append(self)
+
 
     def __str__(self):
         return f'{self.name} | {self.category}'
 
-    name = ''
-    category = ''
-    active = False
+    def list_restaurants():
+        for restaurant in Restaurant.restaurants:
+            print(f'{restaurant.name} | {restaurant.category} | {restaurant.active}')
 
 restaurant_praca = Restaurant('Praça', 'Gourmet')
 restaurant_pizza = Restaurant('Pizza Express', 'Italiana')
 # restaurant_praca.name = 'Praça'
 # restaurant_praca.category = 'Italiana'
 
-restaurants = [restaurant_praca]
+# restaurants = [restaurant_praca]
 
-print(vars(restaurant_praca))
+# print(vars(restaurant_praca))
 
-print(f'Nome: {restaurant_praca.name}')
+# print(f'Nome: {restaurant_praca.name}')
 
-print('Ativo') if restaurant_praca.active else print('Desativado')
+# print('Ativo') if restaurant_praca.active else print('Desativado')
 
-category = Restaurant.category
-Restaurant.name = 'Bistrô'
+# category = Restaurant.category
+# Restaurant.name = 'Bistrô'
 
 # restaurant_pizza = Restaurant()
 # restaurant_pizza.name = 'Pizza Place'
@@ -36,7 +40,9 @@ Restaurant.name = 'Bistrô'
 
 # restaurant_pizza.active = True
 
-print(f'Nome: {restaurant_praca.name} - Categoria: {restaurant_praca.category}')
+# print(f'Nome: {restaurant_praca.name} - Categoria: {restaurant_praca.category}')
 
-print(restaurant_praca)
-print(restaurant_pizza)
+# print(restaurant_praca)
+# print(restaurant_pizza)
+
+Restaurant.list_restaurants()
